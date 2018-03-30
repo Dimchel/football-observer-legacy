9216963b9cee063b96e0f5056f0afbdfadc83753
Dmitriy Kidakov <dimchel7@gmail.com>
2018-03-31 00:06:34 +0300

    finished competitions list unit test

--- app/src/test/java/com/dimchel/footballobserver/CompetitionsListUnitTest.kt
+++ app/src/test/java/com/dimchel/footballobserver/CompetitionsListUnitTest.kt
@@ -1,31 +1,65 @@
 package com.dimchel.footballobserver
 
 import com.dimchel.footballobserver.data.repos.competition.CompetitionRepo
+import com.dimchel.footballobserver.data.repos.competition.models.CompetitionModel
 import com.dimchel.footballobserver.presentation.competitions.CompetitionPresenter
 import com.dimchel.footballobserver.presentation.competitions.CompetitionsView
+import com.dimchel.footballobserver.presentation.competitions.`CompetitionsView$$State`
+import io.reactivex.Single
+import junit.framework.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
-import org.mockito.Mockito
+import org.mockito.*
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.times
+
 
 class CompetitionsListUnitTest {
 
-    private lateinit var presenter: CompetitionPresenter
+    @Mock
     private lateinit var view: CompetitionsView
+
+    @Mock
     private lateinit var repo: CompetitionRepo
+
+    @Mock
+    private lateinit var viewState: `CompetitionsView$$State`
+
+    @Captor
+    private lateinit var captor: ArgumentCaptor<Boolean>
+
+    private lateinit var presenter: CompetitionPresenter
 
     @Before
     fun init() {
-        repo = Mockito.mock(CompetitionRepo::class.java)
-        presenter = Mockito.spy(CompetitionPresenter(repo))
+        MockitoAnnotations.initMocks(this)
 
-        view = Mockito.mock(CompetitionsView::class.java)
+        presenter = CompetitionPresenter(repo)
+        presenter.setViewState(viewState)
     }
 
     @Test
     fun test_progress() {
+        val list: List<CompetitionModel> = listOf(CompetitionModel(
+                1,
+                "1",
+                "1",
+                1,
+                1,
+                1,
+                1,
+                1,
+                "1")
+        )
+
+        `when`(repo.getCompetitionsList()).thenReturn(Single.just(list))
+
         presenter.attachView(view)
 
-        Mockito.verify(view).showProgress(true)
+        Mockito.verify(viewState, times(2)).showProgress(captor.capture())
+        assertEquals(true, captor.allValues[0])
+        assertEquals(false, captor.allValues[1])
+
         Mockito.verify(repo).getCompetitionsList()
     }
 
